0726150e79326121e93f48d1778c1fa780c77cdc
Alan Renner <renner.131@osu.edu>
2020-12-17 18:52:55 -0800

    MongoDB environment variable

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(BodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-var mongoDB = "mongodb://localhost:27017/readio";
+var mongoDB = process.env.MONGODB_URI;
 mongoose
   .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connection successful"));
